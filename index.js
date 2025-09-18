d42110522d4eea7f20d043109d3b0630997223ff
Tobias Engberg <tobias.engberg@exilic.se>
2025-09-18 10:37:49 +0200

    Create elements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+console.log(this);
 var a;
 
 
@@ -26,11 +27,33 @@ function doGreat(){
     //console.log("h: ",h);
 }
 
+let copyText = document.getElementById("targetDivA");
+console.log("CopyText. ", copyText);
+function addElement(elementTag, targetElement, textContent) {
+    let newElement = document.createElement(elementTag);
+    let textValue = document.createTextNode(textContent);
+    newElement.appendChild(textValue);
+    let elementToo = document.getElementById(targetElement);
+    elementToo.appendChild(newElement);
+}
+
+
+addElement("h1", "targetDivA", "Text for h1");
+addElement("p", "targetDivA", "Hello world");
+addElement("h2", "targetDivB", "time for break");
+
 function insertText(){
     let element = document.getElementById("targetDiv");
     element.innerHTML = "Text inserted";
 }
 
 let pressed = document.getElementById("pressable");
-pressed.addEventListener("click", insertText);
+pressed.addEventListener("click", (e) => {
+    let elem = e.srcElement;
+    console.log(e);
+    elem.innerHTML = "Pressed";
+    insertText();
+});
 doGreat();
+
+// () => ccc(56)
